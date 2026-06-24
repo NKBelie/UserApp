76e3ca91cbed7022b93cf408f4b9f798ecdd7bf2
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-24 15:56:50 +0200

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import FetchUsers from "./pages/FetchUsers"
+import AxiosUsers from "./pages/AxiosUsers"
+import ReactQueryUsers from "./pages/ReactQueryUsers"
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-slate-100 text-slate-900">
+      <Navbar />
+      <main className="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/fetch-users" element={<FetchUsers />} />
+          <Route path="/axios-users" element={<AxiosUsers />} />
+          <Route path="/query-users" element={<ReactQueryUsers />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
